35ef310b60a41c63e8860453c0bf552c22b204a6
Juan Martinez <JuanMartinez@Fundacion-jala.local>
2019-08-16 15:10:22 -0400

    Credit card issuer checking

---
+++ csharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace csharp
+{
+    class Test
+    {
+        public string getIssuer(long n)
+        {
+            string number = n.ToString();
+            int size = number.Length;
+            if(number[0] == '4' && (size == 13 || size == 16))
+            {
+                return "VISA";
+            }
+            else if(number[0] == '3' && (number[1] == '4' || number[1] == '7') && size == 15)
+            {
+                return "AMEX";
+            }
+            else if(number[0] == '6' && number[1] == '0' && number[2] == '1' && number[3] == '1' && size == 16)
+            {
+                return "Discover";
+            }
+            else if(number[0] == '5' && (number[1] >= '1' && number[1] <= '5' ) && size == 16)
+            {
+                return "Mastercard";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Test();
+            Console.WriteLine(t.getIssuer(4111111111111111));
+            Console.WriteLine(t.getIssuer(4111111111111));
+            Console.WriteLine(t.getIssuer(4012888888881881));
+            Console.WriteLine(t.getIssuer(378282246310005));
+            Console.WriteLine(t.getIssuer(6011111111111117));
+            Console.WriteLine(t.getIssuer(5105105105105100));
+            Console.WriteLine(t.getIssuer(5105105105105106));
+            Console.WriteLine(t.getIssuer(9111111111111111));
+        }
+    }
+}
--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,7 +1,43 @@
+class Test
+    {
+        String getIssuer(long n)
+        {
+            String number = Long.toString(n);
+            int size = number.length();
+            if(number.charAt(0) == '4' && (size == 13 || size == 16))
+            {
+                return "VISA";
+            }
+            else if(number.charAt(0) == '3' && (number.charAt(1) == '4' || number.charAt(1) == '7') && size == 15)
+            {
+                return "AMEX";
+            }
+            else if(number.charAt(0) == '6' && number.charAt(1) == '0' && number.charAt(2) == '1' && number.charAt(3) == '1' && size == 16)
+            {
+                return "Discover";
+            }
+            else if(number.charAt(0) == '5' && (number.charAt(1) >= '1' && number.charAt(1) <= '5' ) && size == 16)
+            {
+                return "Mastercard";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+    }
 public class Main 
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello Java!");
+        Test t = new Test();
+        System.out.println(t.getIssuer(4111111111111111l));
+        System.out.println(t.getIssuer(4111111111111l));
+        System.out.println(t.getIssuer(4012888888881881l));
+        System.out.println(t.getIssuer(378282246310005l));
+        System.out.println(t.getIssuer(6011111111111117l));
+        System.out.println(t.getIssuer(5105105105105100l));
+        System.out.println(t.getIssuer(5105105105105106l));
+        System.out.println(t.getIssuer(9111111111111111l));
     }
 }
