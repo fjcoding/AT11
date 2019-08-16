1f54bcadc01101903326a882d0d83e1f9072ee7b
Oscar Lopez Alegre <OscarLopez@Fundacion-jala.local>
2019-08-16 09:22:42 -0400

    Oscar

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,87 +1,60 @@
 ﻿using System;
 
 namespace csharp
-{   
+{
     class Program
     {
-
-        static string GetIssuer(Int64 card)
-        {
-            string num = Convert.ToString(card);;
-            int num_aux;
-
-            if ((num.Substring(0,2) == "34" || num.Substring(0, 2) == "37") && (num.Length == 15))
-            {
-                return "AMEX";
-            }
-            else
-            {
-                if (num.Substring(0, 4) == "6011" && num.Length == 16)
-                {
-                    return "DISCOVER";
-                }
-                else
-                {
-                    num_aux = Convert.ToInt32(num.Substring(0, 2));
-                    if ((num_aux>50 && num_aux<56) && (num.Length == 16))
-                    {
-                        return "MASTERCARD";
-                    }
-                    else
-                    {
-                        if (num.Substring(0, 1) == "4" && (num.Length >12 && num.Length <17) )
-                        {
-                            return "VISA";
-                        }
-                        else
-                        {
-                            return "UNKNOWN";
-                        }
-                    }
-                }
-            }
-        }
-        static string getIssuer(long longNumber)
-        {
-            string number = longNumber.ToString();
-            char firstDigit = number[0];
-            char secondDigit = number[1];
-            char thirdDigit = number[2];
-            char fourthDigit = number[3];
-            int size = number.Length;
-            if(firstDigit == '4' && (size == 13 || size == 16))
-            {
-                return "VISA";
-            }
-            else if(firstDigit == '3' && (secondDigit == '4' || secondDigit == '7') && size == 15)
-            {
-                return "AMEX";
-            }
-            else if(firstDigit == '6' && secondDigit == '0' && thirdDigit == '1' && fourthDigit == '1' && size == 16)
-            {
-                return "Discover";
-            }
-            else if(firstDigit == '5' && (secondDigit >= '1' && secondDigit <= '5' ) && size == 16)
-            {
-                return "Mastercard";
-            }
-            else
-            {
-                return "Unknown";
-            }
-        }
-
         static void Main(string[] args)
         {
-            // var t = new Test();
-            Console.WriteLine(getIssuer(4111111111111111));
-            Console.WriteLine(getIssuer(4111111111111));
-            Console.WriteLine(getIssuer(4012888888881881));
-            Console.WriteLine(getIssuer(378282246310005));
-            Console.WriteLine(getIssuer(6011111111111117));
-            Console.WriteLine(getIssuer(5105105105105100));
-            Console.WriteLine(getIssuer(5105105105105106));
-            Console.WriteLine(getIssuer(9111111111111111));
+            Console.WriteLine("Ingrese numero de tarjeta de credito");
+            string numeroTarjeta = Console.ReadLine();
+            Console.WriteLine(getIssuer(numeroTarjeta));
+
         }
+
+        public static string getIssuer(string tarjeta)
+        {
+            string op1 = tarjeta[0].ToString();
+            string op2 = tarjeta[1].ToString();
+            switch (op1)
+            {
+                case "4":
+                    {
+                        if ((tarjeta.Length==16) || (tarjeta.Length==13))
+                        {
+                            return "Tarjeta Visa";
+                        }
+                        break;
+                    }
+                case "3":
+                    {
+                        if (((op2=="4") || (op2 == "7")) && (tarjeta.Length == 15))
+                        {
+                            return "Tarjeta AMEX";
+                        }
+                        break;
+                    }
+                case "5":
+                    {
+                        if (((op2 == "1") || (op2 == "2") || (op2 == "3") || (op2 == "4") || (op2 == "5")) && (tarjeta.Length == 16))
+                        {
+                            return "Tarjeta MasterCard";
+
+                        }
+                        break;
+                    }
+                case "6":
+                    {
+                        if (((tarjeta[1].ToString() == "0") && (tarjeta[2].ToString() == "1") || (tarjeta[3].ToString() == "1") ) && (tarjeta.Length == 16))
+                        {
+                            return "Tarjeta Discovery";
+
+                        }
+                        break;
+                    }
+            }
+            return "Unknown";
+        }
+
     }
 }
