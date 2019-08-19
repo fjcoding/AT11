c9f75688f9afa0034a072306aabef40de45c6424
Bruno Burgos Urey <BrunoBurgos@Fundacion-jala.local>
2019-08-19 16:52:17 -0400

    Bruno's commit

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,7 +1,46 @@
-public class Main 
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello Java!");
-    }
+public class Main {
+	//Credit Card
+	public static void main (String[] args){
+
+		//String texto="378282246310005";
+		//String texto="4012888888881881";
+		//String texto="5105105105105100";
+		String texto="6011111111111117";
+		System.out.println(getIssuer(texto));
+	}
+
+	public static String getIssuer (String texto){
+		int size_number=texto.length();
+		String answer="none";
+		char first_letter=texto.charAt(0); //Character
+		char second_letter=texto.charAt(1);
+		switch (first_letter){
+			case '3':
+			if (size_number==15){
+				if (second_letter=='4' || second_letter=='7')
+					answer="AMEX";
+			}
+			break;
+			case '4':
+			if (size_number==13 || size_number==16)
+				answer="VISA";
+			break;
+			case '5':
+			if (size_number==16){
+				if (second_letter=='1' || second_letter=='2' || second_letter=='3' || second_letter=='4' || second_letter=='5')
+					answer="Mastercard";
+			}
+			break;
+			case '6':
+			if (size_number==16)
+				answer="Discover";
+			break;
+			default:
+				answer="Unknown";
+			break;
+			
+		}// switch 
+		return answer;
+	}// function
 }
+
