fad3fccd55e58a29fb6f0f2ffb887995b8126140
Oscar Lopez Alegre <OscarLopez@Fundacion-jala.local>
2019-08-20 16:31:36 -0400

    Correciones Oscar

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -6,55 +6,52 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese numero de tarjeta de credito");
-            string numeroTarjeta = Console.ReadLine();
-            Console.WriteLine(getIssuer(numeroTarjeta));
-
+            Console.WriteLine("Enter Credit Card number");
+            string numberCard = Console.ReadLine();
+            Console.WriteLine(getIssuer(numberCard));
         }
 
-        public static string getIssuer(string tarjeta)
+        public static string getIssuer(string numberCard)
         {
-            string op1 = tarjeta[0].ToString();
-            string op2 = tarjeta[1].ToString();
+            string op1 = numberCard[0].ToString();
+            string op2 = numberCard[1].ToString();
+
             switch (op1)
             {
                 case "4":
+                {
+                    if ((numberCard.Length==16) || (numberCard.Length==13))
                     {
-                        if ((tarjeta.Length==16) || (tarjeta.Length==13))
-                        {
-                            return "Tarjeta Visa";
-                        }
-                        break;
+                        return "Credit Card Visa";
                     }
+                break;
+                }
                 case "3":
+                {
+                    if (((op2=="4") || (op2 == "7")) && (Credit Card.Length == 15))
                     {
-                        if (((op2=="4") || (op2 == "7")) && (tarjeta.Length == 15))
-                        {
-                            return "Tarjeta AMEX";
-                        }
-                        break;
+                        return "Credit Card AMEX";
                     }
+                break;
+                }
                 case "5":
+                {
+                    if (((op2 == "1") || (op2 == "2") || (op2 == "3") || (op2 == "4") || (op2 == "5")) && (numberCard.Length == 16))
                     {
-                        if (((op2 == "1") || (op2 == "2") || (op2 == "3") || (op2 == "4") || (op2 == "5")) && (tarjeta.Length == 16))
-                        {
-                            return "Tarjeta MasterCard";
-
-                        }
-                        break;
+                        return "Credit Card MasterCard";
                     }
+                break;
+                }
                 case "6":
+                {
+                    if (((numberCard[1].ToString() == "0") && (numberCard[2].ToString() == "1") || (numberCard[3].ToString() == "1") ) && (numberCard.Length == 16))
                     {
-                        if (((tarjeta[1].ToString() == "0") && (tarjeta[2].ToString() == "1") || (tarjeta[3].ToString() == "1") ) && (tarjeta.Length == 16))
-                        {
-                            return "Tarjeta Discovery";
-
-                        }
-                        break;
+                        return "Credit Card Discovery";
                     }
+                break;
+                }
             }
             return "Unknown";
         }
-
     }
 }
