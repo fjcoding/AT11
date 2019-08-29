c23d4027266feff4ea73f7ec2c291ace8268db8d
Juan Martinez <JuanMartinez@Fundacion-jala.local>
2019-08-29 17:45:50 -0400

    Second Test Update FirstName Return FullName

--- csharp/bankocr.Tests/TestPerson.cs
+++ csharp/bankocr.Tests/TestPerson.cs
@@ -14,9 +14,21 @@ namespace TestPerson.Tests
         [Fact]
         public void WhenIPutFirstLastNameReturnFullName()
         {
-            Person result = _person.NamedOne("Jhon", "Due");
-            string expected = "Jhon Due";
+            Person result = _person.NamedOne("Naomi","Wang");
+            string expected = "Naomi Wang";
             Assert.Equal(expected, result.fullName);
+        }
+        [Fact]
+        public void WhenIUpdateFirstNameReturnFullName()
+        {
+            Person person = _person.NamedOne("Naomi","Wang");
+            person.setFirstName("John");
+            person.setFullName();
+
+            string expected = "John Wang";
+            string result = person.getFullName();
+
+            Assert.Equal(expected,result);
         }
     }
 }
--- csharp/bankocr/Person.cs
+++ csharp/bankocr/Person.cs
@@ -13,8 +13,25 @@ namespace bankocr
             person.firstName = inputFirstName;
             person.lastName = inputLastName;
             person.fullName = person.firstName + " " + person.lastName;
-            
+
             return person;
+        }
+
+        public void setFirstName(string inputFirstName)
+        {
+            this.firstName = inputFirstName;
+        }
+        public void setLastName(string inputLastName)
+        {
+            this.lastName = inputLastName;
+        }
+        public void setFullName()
+        {
+            this.fullName = this.firstName + " " + this.lastName;
+        }
+        public string getFullName()
+        {
+            return this.fullName;
         }
     }
 }
